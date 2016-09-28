89f572b682a33e7411179412d65dadb6e997796b
s1519 <s1519>
2016-09-28 12:20:07 +0900

    add kadai3

--- kadai3.rb
+++ kadai3.rb
@@ -1,6 +1,5 @@
 #encoding: utf-8
 class Vector
-  attr_reader :a, :b
   def initialize(a,b)
   	@a = a
   	@b = b
@@ -13,16 +12,9 @@ class Vector
   def length
   	num = Math::sqrt(@a**2+@b**2)
   end
-
-  def add(v)
-  	Vector.new(@a + v.a,@b + v.b)
-  end
 end
 
 #irb で実行が基本
 v1 = Vector.new(1,2)
 v1.to_s
 v1.length
-v2 = Vector.new(2,3)
-v3 = v1.add(v2)
-puts v3
